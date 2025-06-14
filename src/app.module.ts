00d29e55ad253b249ffdf07937df9f4414033a98
Your Name <you@example.com>
2025-06-14 23:31:46 +0500

    feat: добавлен ConfigModule, ValidationPipe, CORS и Swagger-документация
    - Подключён и настроен ConfigModule для работы с переменными окружения.
    - Реализована базовая настройка окружения с поддержкой .env и .env.production.
    - Добавлен ValidationPipe для глобальной валидации и трансформации входящих данных.
    - Настроен CORS с поддержкой указания разрешённого источника, методов и учётных данных.
    - Интегрирована Swagger-документация с поддержкой Bearer токенов и профессиональным описанием API.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { IS_DEV_ENV } from './libs/common/utils/is-dev.utils';
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true, // Makes the configuration available globally ],
+    ignoreEnvFile:!IS_DEV_ENV, // Ignores .env file in production
+    envFilePath: IS_DEV_ENV ? '.env' : '.env.production', // Loads different .env files based on the environment
+  })],
+
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
